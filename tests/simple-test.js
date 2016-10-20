191556d4ea0dd4a0cbef56f6b2fc4dd65de8520f
Rob Haley <rob_haley@live.co.uk>
2016-10-20 13:53:27 +0100

    add initial testing infrastructure

---
+++ tests/simple-test.js
@@ -0,0 +1,8 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('some functionality', function() {
+    it('should work', function() {
+        expect(2+2).to.equal(2*2);
+    });
+});
