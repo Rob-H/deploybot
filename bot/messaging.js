116fbd6fab0da5b1b38cc8718ffcf34f7ba08f96
Rob Haley <rob_haley@live.co.uk>
2016-10-21 12:57:49 +0100

    add initial scaffolding for test

---
+++ bot/messaging.js
@@ -0,0 +1,6 @@
+'use strict';
+module.exports = {
+    receive: () => undefined,
+    send: () => undefined   
+};
+
--- tests/simple-test.js
+++ tests/simple-test.js
@@ -1,8 +1,35 @@
+'use strict';
 const chai = require('chai');
 const expect = chai.expect;
+const sinon = require('sinon');
+const git = require('./helpers/git.js');
+const messaging = require('../bot/messaging.js');
+const deployObserver = require('../bot/deployObserver.js');
 
-describe('some functionality', function() {
-    it('should work', function() {
-        expect(2+2).to.equal(2*2);
+describe('the bot', function() {
+    describe('when there are some commits', function() {
+        beforeEach(function() {
+            this.commits = (new Array(10).map(git.emptyCommit));
+            this.userToken = 'robh';
+            sinon.spy(messaging, 'send');
+        });
+
+        afterEach(function() {
+            messaging.send.restore();
+        });
+
+        describe('and someone asks me to remind them when a commit is deployed', function() {
+            beforeEach(function() {
+                messaging.receive(this.userToken, `remind me when ${this.commits[9]} is deployed`)
+            });
+            describe('when a commit before that is deployed', function() {
+                beforeEach(function() {
+                    deployObserver.notify(this.commits[5]);
+                });
+                it('does not send them a message', function() {
+                    expect(messaging.send.called).to.be.false
+                });
+            });
+        });
     });
 });
