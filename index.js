be1400a7f87323dd13a24d904d346caf9e7fef13
Rob Haley <rob_haley@live.co.uk>
2016-11-26 23:33:56 +0000

    use bunyan for logging

--- index.js
+++ index.js
@@ -7,7 +7,16 @@ const responder = require('./bot/responder.js');
 const deployObserver = require('./bot/deployObserver.js');
 const express = require('express');
 const bodyParser = require('body-parser');
+const bunyan = require('bunyan');
 
+const log = bunyan.createLogger({
+    name: 'deploy-bot',
+    streams: [{
+        type: 'rotating-file',
+        path: 'logs/deploy-bot.log',
+        period: '1d',
+    }]
+});
 
 if (!process.env.slackToken) {
     console.log('Error: Specify slackToken in environment');
@@ -37,16 +46,17 @@ git.initAtLocation('repository', process.env.gitRepoUrl, git.getCreds(process.en
         const send = (user, message) => {
             message.channel = user;
             bot.say(message, function(err, response) {
-                if(err) console.log(err); 
-                if(response) console.log(response); 
+                if(err) log.error(err); 
+                if(response) log.info({response}, 'received response'); 
             });    
         };
 
         controller.on('direct_message',function(bot,message) {
+            log.info({message}, 'recieved message');
             responder(gitObj, store, environments).handleMessage(message.channel, message.text)
                 .then(response => bot.reply(message, response))
                 .catch(err => {
-                    console.log(err); 
+                    if(err) log.error(err); 
                     bot.reply(message, 'sorry something went wrong contact your sysadmin!!');
                 });
         });
@@ -55,18 +65,21 @@ git.initAtLocation('repository', process.env.gitRepoUrl, git.getCreds(process.en
         app.use(bodyParser.json());
 
         app.post('/', function(req, res) {
-            console.log('received', req.body);
+            log.info('received', req.body);
             deployObserver(send, gitObj, store, environments)
                 .notify(req.body.environment, req.body.commitHash)
                 .then((messagesSent) => res.status(200).send(`sent ${messagesSent} messages`))
                 .catch((err) => {
                     if(err.message.includes('Unrecognised environment')) res.status(400).send(err.message);
-                    else res.status(500).send(err.message);
+                    else {
+                        log.error(err); 
+                        res.status(500).send(err.message);
+                    }
                 });
         });
 
-        app.listen(8080, () => console.log(`listening for deployment notifications`));
+        app.listen(8080, () => log.info(`listening for deployment notifications`));
 
     })
-    .catch(err => console.log(err));
+    .catch(err => log.error(err));
 
